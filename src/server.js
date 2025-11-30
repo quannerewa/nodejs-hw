9a4776f57cdcf7867d1e3358236a2a47601f8d1f
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-11-30 16:15:18 +0200

    upd

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
 import helmet from 'helmet';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 
@@ -10,63 +14,28 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // standart middleware
+app.use(logger);
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }));
+app.use(notesRoutes);
 
 // routes
-
-// Отримати всі нотатки
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-// Отримати нотатку за ID
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-// Тестовий маршрут для перевірки помилки
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to Notes API 🚀',
-    availableRoutes: ['/notes', '/notes/:noteId', '/test-error']
+    availableRoutes: ['/notes', '/notes/:noteId']
   });
 });
 
 
 // 404 неіснуючі маршрути
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+app.use(notFoundHandler);
+// error handler
+app.use(errorHandler);
 
-//
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  const isProd = process.env.NODE_ENV === 'production';
-  res.status(500).json({
-    message: isProd ? 'Something went wrong, please try again later.'
-      : err.message,
-  });
-});
+//  start & conect to db
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
