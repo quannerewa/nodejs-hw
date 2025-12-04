c880702b14694fecbe1f2f968bef5bbf76306d14
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-12-04 10:20:45 +0200

    upd hw 4

--- src/models/note.js
+++ src/models/note.js
@@ -18,6 +18,12 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   {
     timestamps: true,
@@ -27,3 +33,4 @@ const noteSchema = new Schema(
 noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
+
