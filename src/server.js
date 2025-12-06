9e71ebcd668251bc398fbfc92e2b31b8d219f8fe
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-12-06 16:50:04 +0200

    upd 05-mail-and-img

--- src/server.js
+++ src/server.js
@@ -1,46 +1,34 @@
 import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import 'dotenv/config';
+import cors from 'cors';
 import { errors } from 'celebrate';
-
+import cookieParser from 'cookie-parser';
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
-import authRoutes from './routes/authRoutes.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// middleware
 app.use(logger);
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
-// роутинг
-app.use('/auth', authRoutes);
-app.use('/notes', notesRoutes);
+app.use(authRoutes);
+app.use(notesRoutes);
+app.use(userRoutes);
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to Notes API 🚀',
-    availableRoutes: ['/auth/register', '/auth/login', '/notes'],
-  });
-});
-
-// 404
 app.use(notFoundHandler);
-
-// celebrate errors
 app.use(errors());
-
-// error handler
 app.use(errorHandler);
 
-// start server
 await connectMongoDB();
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/utils/saveFileToCloudinary.s
@@ -0,0 +1,42 @@
+import { Readable } from 'node:stream';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+/*
+console.log({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? '******' : null
+});
+*/
+
+export async function saveFileToCloudinary(buffer) {
+  //console.log;
+
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'notehub-app/avatars',
+        resource_type: 'image',
+        overwrite: true,
+        unique_filename: true,
+        use_filename: false,
+      },
+      (err, result) => (err ? reject(err) : resolve(result))
+    );
+
+    const readable = new Readable();
+    readable._read = () => {};
+    readable.push(buffer);
+    readable.push(null);
+
+    readable.pipe(uploadStream);
+    //console.log;
+
+  });
+}
