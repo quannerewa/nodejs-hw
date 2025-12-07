2ffec3eb905c8785473a0323a08adc621c17e7b7
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-12-07 17:55:07 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -21,7 +20,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
-app.use(userRoutes);
+
 
 app.use(notFoundHandler);
 app.use(errors());
